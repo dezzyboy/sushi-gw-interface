1302e81954fee55e63fb7dd47995dbdb1c176eb2
dezzyboy <dezzyboy@tradebit.ca>
2022-04-15 17:18:40 -0400

    chore: add tokens to commonbases

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -1,13 +1,13 @@
 // a list of tokens by chain
 import { ChainId, SUSHI, Token, WNATIVE } from '@dezzyboy/sushiswap-core-sdk'
 
-import * as GODWOKEN from './tokens/arbitrum'
 import * as AVALANCHE from './tokens/avalanche'
 import * as BSC from './tokens/bsc'
 import * as CELO from './tokens/celo'
 import * as ETHEREUM from './tokens/ethereum'
 import * as FANTOM from './tokens/fantom'
 import * as FUSE from './tokens/fuse'
+import * as GODWOKEN from './tokens/godwoken'
 import * as HARMONY from './tokens/harmony'
 import * as HECO from './tokens/heco'
 import * as MATIC from './tokens/matic'
